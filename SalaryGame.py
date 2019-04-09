844f7d4349d5b4e8ddf8f33f2cc225bcd07f6058
cfp106022204 <48315312+cfp106022204@users.noreply.github.com>
2019-04-10 01:06:44 +0800

    Update and rename SalaryGame.ipynb to SalaryGame.py

---
+++ SalaryGame.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+n,m,k,dm=200,40,30,10
+money=np.linspace(m,m,n)
+for i in range(k):
+    p1=p2=0
+    while (p1==p2 or money[p1]==0 or money[p2]==0):
+        p1=np.random.randint(0,n)
+        p2=np.random.randint(0,n)
+    
+    win=np.random.randint(0,1)
+    if win==0:
+        money[p1]-=dm
+        money[p2]+=dm
+    else:
+        money[p1]+=dm
+        money[p2]-=dm
+
+bins=10
+plt.clf()
+plt.hist(money,bins)
+plt.ylim(0,n)
+plt.show()
